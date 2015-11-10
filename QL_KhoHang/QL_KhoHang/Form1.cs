52987628e7c78d32e26bc8a214febaad7f05059e
dungtienmta <dungtienmta@gmail.com>
2015-11-10 15:07:41 +0700

    update_Form

--- QL_KhoHang/QL_KhoHang/Form1.cs
+++ QL_KhoHang/QL_KhoHang/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLogic;
 
 namespace QL_KhoHang
 {
@@ -16,5 +17,21 @@ namespace QL_KhoHang
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            HienThi();
+        }
+        HangHoa hh = new HangHoa();
+        public void HienThi()
+        {
+            dataGridView1.DataSource = hh.ShowHangHoa("");
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                dataGridView1.Rows[i].Cells[0].Value = (i + 1).ToString();
+            }
+        }
+
+        
     }
 }
